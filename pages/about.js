23785af4718ebf35239407d0d1f6f40ff17ebc1c
Thi Nguyen <dnguyen209@my.bcit.ca>
2023-01-24 10:57:38 -0800

    class 4 example

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function About() {
+    return (
+        <div>
+            <div>About</div>
+            <Link href="http://localhost:3000/">Go Back</Link>
+        </div>
+    )
+}
